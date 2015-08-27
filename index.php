a23767a756105d9df45f62954a59f6a7a15fd240
smitevils <smitevils@yandex.ru>
2015-08-27 12:53:08 +0300

    add styles files

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Phenix</title>
+	<link rel="stylesheet" href="assets/styles/normalize.css">
+	<link rel="stylesheet" href="assets/styles/main.css">
+</head>
+<body>
+	<script src="assets/scripts/js/jquery-2.1.4.min.js"></script>
+</body>
+</html>
